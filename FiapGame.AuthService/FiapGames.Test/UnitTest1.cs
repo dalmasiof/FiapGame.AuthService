bfca9accb70e814819c798238770f8895e9c1817
Dalmasio <dalma@MAIN-PC>
2026-05-31 15:28:18 -0300

    estrutura inicial

---
+++ FiapGame.AuthService/FiapGames.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace FiapGames.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
